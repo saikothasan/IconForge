457a88eeb03ed7511bd2bb77aafe94c4a38a6bda
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-02-04 21:16:48 +0600

    Update route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -43,6 +43,7 @@ export async function POST(request: Request) {
             width={size.width}
             height={size.height}
             style={{ objectFit: "contain" }}
+            alt="Favicon"
           />
         </div>,
         {
